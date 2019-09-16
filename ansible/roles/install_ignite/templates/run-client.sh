d18a0a7e8f28c5125e461c9df966e89818b4f9c6
Ryzhov Sergei <s.vi.ryzhov@gmail.com>
2019-09-16 15:15:41 +0300

    edit

--- ansible/roles/install_ignite/templates/run-client.sh
+++ ansible/roles/install_ignite/templates/run-client.sh
@@ -5,5 +5,9 @@ export IGNITE_HOME="{{ ignite_home }}"
 
 JVM_OPTS="$JVM_OPTS -Dnode.id=`hostname`"
 
+ps aux | grep config-client.xml | awk '{print $2}' | xargs kill -9
+
+sleep 10
+
 nohup sh $IGNITE_HOME/bin/ignite.sh "{{ config_client }}" > /dev/null 2>&1 &
 
